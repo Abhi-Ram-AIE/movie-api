c253fe3aae592595e8ed0d2e45b431b9826a5ae8
Abhi-Ram-AIE <gobburiabhiram@gmail.com>
2025-04-10 23:35:43 +0530

    one final act

--- cinema/src/main/java/dev/abhiram/cinema/CorsConfig.java
+++ cinema/src/main/java/dev/abhiram/cinema/CorsConfig.java
@@ -1,13 +1,9 @@
 package dev.abhiram.cinema;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -17,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://movie-frontenddb.netlify.app")
+                        .allowedOrigins(
+                                "https://movie-frontenddb.netlify.app",
+                                "https://lighthearted-gnome-934d4e.netlify.app"
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
