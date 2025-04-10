a7ef21b2bdcbc510f1a5bfc392c23f7c2d856929
Abhi-Ram-AIE <gobburiabhiram@gmail.com>
2025-04-10 21:41:01 +0530

    Fix: CORS

--- cinema/src/main/java/dev/abhiram/cinema/CinemaController.java
+++ cinema/src/main/java/dev/abhiram/cinema/CinemaController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://movie-frontenddb.netlify.app")
 @RestController
 @RequestMapping("/api/v1/cinema")
 
